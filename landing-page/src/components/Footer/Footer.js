2222173976348670715a0fedd8e1f341d39f78ca
Dan Carlsonm <dcarlson723@gmail.com>
2025-01-23 20:35:41 -0500

    Refactor Benefits and Footer components for improved styling; update HowItWorks component layout and content

--- landing-page/src/components/Footer/Footer.js
+++ landing-page/src/components/Footer/Footer.js
@@ -5,7 +5,11 @@ import './Footer.css';
 export default function Footer () {
     return ( 
         <div className="FooterBackground">
-            Footer
+            Privacy Policy
+            Terms of service
+            Cookie Policy
+
+            © 2024 CloudFab Inc. All rights reserved.
         </div>
     )
 }
--- landing-page/src/components/HowItWorks/HowItWorks.js
+++ landing-page/src/components/HowItWorks/HowItWorks.js
@@ -12,81 +12,48 @@ export default function HowItWorks () {
     return ( 
         <div className='BackgroundContainer'>
             <div className='UIContainer'>   
-                    <div className='LeftSide'>
-                        <h1>How it works</h1>
-                    </div>
-                    <div className='RightSide'>
-                        <div className='StepCard'>
+                <div className='LeftSide'>
+                    <h1>How it works</h1>
+                    {/* <img className='HomeCardImg' src={CADMachine} alt=''></img> */}
+                </div>
+                <div className='RightSide'>
+                    <div className='StepCard'>
+                        <div className='StepCardLeft'>
                             <div className='Number'>1</div>
                             <div className='StepText'>Upload your model </div>
-                            <div className='SecondaryStepText'>as a STEP from your preffered CAD software</div>
                         </div>
-                        <div className='StepCard'>
+                        <div className='SecondaryStepText'>as a STEP from your preffered CAD software</div>
+                    </div>
+                    <div className='StepCard'>
+                        <div className='StepCardLeft'>
                             <div className='Number'>2</div>
                             <div className='StepText'>Choose your stock </div>
-                            <div className='SecondaryStepText'>from CloudFabs available materials</div>
                         </div>
-                        <div className='StepCard'>
+                        <div className='SecondaryStepText'>from CloudFabs available materials</div>
+                    </div>
+                    <div className='StepCard'>
+                        <div className='StepCardLeft'>
                             <div className='Number'>3</div>
                             <div className='StepText'>Fixture your material </div>
-                            <div className='SecondaryStepText'>using CloudFabs proprietary software</div>
                         </div>
-                        <div className='StepCard'>
+                        <div className='SecondaryStepText'>using CloudFabs proprietary software</div>
+                    </div>
+                    <div className='StepCard'>
+                        <div className='StepCardLeft'>
                             <div className='Number'>4</div>
                             <div className='StepText'>Programs your tool paths </div>
-                            <div className='SecondaryStepText'>using industry standard pathing and operations</div>
                         </div>
-                        <div className='StepCard'>
+                        <div className='SecondaryStepText'>using industry standard pathing</div>
+                    </div>
+                    <div className='StepCard'>
+                        <div className='StepCardLeft'>
                             <div className='Number'>5</div>
                             <div className='StepText'>Save money on your parts </div>
-                            <div className='SecondaryStepText'>and gets your parts made fast</div>
                         </div>
+                        <div className='SecondaryStepText'>and gets your parts made fast</div>
                     </div>
-            </div>
-
-
-
-
-
-
-            {/* <h1 id="howitworks">How it works</h1>
-
-            <div className='IntroGallery'>
-                <div className='HomeCard'>
-                    <div className='HomeCardText'>Upload Your Model</div>
-                    <img className='HomeCardImg' src={CADModel} alt=''></img>
-                </div>
-                <div className='ArrowImg'>
-                    <img src={ArrowImg} alt=''></img>
-                </div>
-                <div className='HomeCard'>
-                    <div className='HomeCardText'>Program Your Part</div>
-                    <img className='HomeCardImg' src={CADMachine} alt=''></img>
-                </div>
-                <div className='ArrowImg'>
-                    <img src={ArrowImg} alt=''></img>
-                </div>
-                <div className='HomeCard'>
-                    <div className='HomeCardText'><p>Save <span className="MoneyText">$$$</span> on Quality Parts</p></div>
-                    <img className='HomeCardImg' src={CADPart} alt=''></img>
                 </div>
             </div>
-            <div className='HowItWorksText'>
-                <p>Pick your stock</p>
-                <p>Fixture your part</p>
-                <p>Probe your stock</p>
-                <p>Path your tools</p>
-                <p>Simulate your job</p>
-                <p>Submit for production</p>
-            </div> */}
-
-
-
-
-
-
-
-
         </div>
     )
 }
