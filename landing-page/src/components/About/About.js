cb22d08b41dfc25a5dcb19b6a8033fc76627c040
djc2105 <DCarlson19@shschools.org>
2025-01-30 18:49:50 -0500

    Update About component text for clarity and accuracy; adjust second text color in CSS

--- landing-page/src/components/About/About.js
+++ landing-page/src/components/About/About.js
@@ -19,8 +19,10 @@ export default function About() {
                 
                     <AccordionDetails className='AccordionDetailsClass'>
                         We are a small team of engineers that are frustrated by the lack of 
-                        access to fabrication tools and machines for personal projects, university projects, startup ideas, 
-                        and prototyping generally.
+                        access to fabrication tools and machines for low volume manufacturing for machined components.
+                        This has prevented us from developing personal projects, university projects, startup ideas, and limited access prototyping generally.
+                        CloudFab will allow engineers and makers to access CNC macnufacturing at a fraction of the cost of traditional job shops and online 
+                        machining servies.
                     </AccordionDetails>
                 </Accordion>
             
@@ -30,7 +32,7 @@ export default function About() {
                     </AccordionSummary>
                     <AccordionDetails  className='AccordionDetailsClass'>
                         Other companies will provide parts from a 3D model at a high cost, but we believe that engineers 
-                        and makers in 2024 are capable of much more. Through the CloudFab platform, we allow engineers to interface 
+                        and makers in 2024 are capable of much more. Through the CloudFab platform, we allow users to interface 
                         directly with the tools they are using, putting the power of manufacturing back into your hands and saving 
                         you money at the same time.
                     </AccordionDetails>
@@ -41,6 +43,7 @@ export default function About() {
                         What services we offer:
                     </AccordionSummary>
                     <AccordionDetails className='AccordionDetailsClass'>
+                        Currently, We are focusing on enabling low cost CNC milling and turining.
                         In the future, we plan to offer a full range of fabrication and manufacturing services including 3D printing, 
                         sheet metal fabrication, tube bending and much, much more.
                     </AccordionDetails>
