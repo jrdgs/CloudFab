dd1c48c78e1d1dae296fec7eb9832cd672822e0c
Dan Carlsonm <dcarlson723@gmail.com>
2025-01-30 18:27:19 -0500

    Changed about page

--- landing-page/src/components/About/About.js
+++ landing-page/src/components/About/About.js
@@ -10,41 +10,46 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 export default function About() {
   return (
     <div className="BackgroundContainer" id="about">
-        <h1>About Us</h1>
-    
-        <Accordion className='AccordionClass'>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />} className='AccordionSummaryClass'>
-                Who are we:
-            </AccordionSummary>
-        
-            <AccordionDetails className='AccordionDetailsClass'>
-                We are a small team of engineers that are frustrated by the lack of 
-                access to fabrication tools and machines for personal projects, university projects, startup ideas, 
-                and prototyping generally.
-            </AccordionDetails>
-        </Accordion>
-        
-        <Accordion className='AccordionClass'>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />} className='AccordionSummaryClass'>
-                What makes us different:
-            </AccordionSummary>
-            <AccordionDetails  className='AccordionDetailsClass'>
-                Other companies will provide parts from a 3D model at a high cost, but we believe that engineers 
-                and makers in 2024 are capable of much more. Through the CloudFab platform, we allow engineers to interface 
-                directly with the tools they are using, putting the power of manufacturing back into your hands and saving 
-                you money at the same time.
-            </AccordionDetails>
-        </Accordion>
-      
-        <Accordion className='AccordionClass'>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />} className='AccordionSummaryClass'>
-                What services we offer:
-            </AccordionSummary>
-            <AccordionDetails className='AccordionDetailsClass'>
-                In the future, we plan to offer a full range of fabrication and manufacturing services including 3D printing, 
-                sheet metal fabrication, tube bending and much, much more.
-            </AccordionDetails>
-        </Accordion>
+        <div className="UIContainer">
+            <div className='LeftSide'>
+                <Accordion className='AccordionClass'>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />} className='AccordionSummaryClass'>
+                        Who are we:
+                    </AccordionSummary>
+                
+                    <AccordionDetails className='AccordionDetailsClass'>
+                        We are a small team of engineers that are frustrated by the lack of 
+                        access to fabrication tools and machines for personal projects, university projects, startup ideas, 
+                        and prototyping generally.
+                    </AccordionDetails>
+                </Accordion>
+            
+                <Accordion className='AccordionClass'>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />} className='AccordionSummaryClass'>
+                        What makes us different:
+                    </AccordionSummary>
+                    <AccordionDetails  className='AccordionDetailsClass'>
+                        Other companies will provide parts from a 3D model at a high cost, but we believe that engineers 
+                        and makers in 2024 are capable of much more. Through the CloudFab platform, we allow engineers to interface 
+                        directly with the tools they are using, putting the power of manufacturing back into your hands and saving 
+                        you money at the same time.
+                    </AccordionDetails>
+                </Accordion>
+            
+                <Accordion className='AccordionClass'>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />} className='AccordionSummaryClass'>
+                        What services we offer:
+                    </AccordionSummary>
+                    <AccordionDetails className='AccordionDetailsClass'>
+                        In the future, we plan to offer a full range of fabrication and manufacturing services including 3D printing, 
+                        sheet metal fabrication, tube bending and much, much more.
+                    </AccordionDetails>
+                </Accordion>
+            </div>
+            <div className='RightSide'>
+                <h1>About Us</h1>                           
+            </div>
+        </div>
     </div>
   );
 }
