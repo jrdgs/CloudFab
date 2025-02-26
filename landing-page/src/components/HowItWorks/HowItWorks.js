0689f2241e6dc758b824dba4df59165f068e6ed5
John Rodrigues <rodrigues.joh@northeastern.edu>
2025-02-25 20:28:32 -0500

    Responsive Timeline

--- landing-page/src/components/HowItWorks/HowItWorks.js
+++ landing-page/src/components/HowItWorks/HowItWorks.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './HowItWorks.css';
 import '../../pages/HomePage/HomePage.css';
 import Timeline from '@mui/lab/Timeline';
@@ -19,8 +19,18 @@ const theme = createTheme({
     },
 });
 
-
 export default function HowItWorks() {
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    // Update screen width on resize
+    useEffect(() => {
+        const handleResize = () => setScreenWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     const steps = [
         { title: "Upload your model", desc: "as a STEP from your preferred CAD software" },
         { title: "Choose your stock", desc: "from CloudFabs available materials" },
@@ -32,101 +42,57 @@ export default function HowItWorks() {
     return (
         <ThemeProvider theme={theme}>
             <div className="BackgroundContainer" id="howitworks">
-                <div className="UIContainer">
+                <div className={`UIContainer ${screenWidth < 900 ? 'StackedLayout' : ''}`}>
                     <div className="LeftSide">
                         <h1>How it works</h1>
                     </div>
                     <div className="RightSide">
-                        <Timeline position="alternate-reverse" sx={{ marginTop: "100px" }}>
-                            {steps.map((step, index) => (
-                                <TimelineItem key={index} sx={{ display: 'flex', alignItems: 'center' }}>
-                                    <TimelineSeparator sx={{ height: "1in" }} >
-                                        <TimelineDot variant="filled"
-                                            sx={{
-                                                backgroundColor: theme.palette.primary.accent,
-                                                borderColor: theme.palette.primary.accent,
-                                                borderWidth: 1,
-                                                borderStyle: "solid"
-                                            }} />
-                                        {index !== steps.length - 1 && (
-                                            <TimelineConnector
-                                                sx={{
-                                                    bgcolor: theme.palette.primary.main,
-                                                }}
-                                            />
-                                        )}
-                                    </TimelineSeparator>
-                                    <TimelineContent>
+                        {screenWidth < 900 ? (
+                            <div className="timeline-wrapper">
+                                {steps.map((step, index) => (
+                                    <div key={index} className="timeline-step">
                                         <div className="timeline-content">
                                             <h2>{step.title}</h2>
                                             <p>{step.desc}</p>
                                         </div>
-                                    </TimelineContent>
-                                </TimelineItem>
-                            ))}
-                        </Timeline>
+                                        {index !== steps.length - 1 && (
+                                            <div className="timeline-divider" />
+                                        )}
+                                    </div>
+                                ))}
+                            </div>
+                        ) : (
+                            <Timeline position="alternate" sx={{ marginTop: screenWidth < 900 ? "50px" : "100px" }}>
+                                {steps.map((step, index) => (
+                                    <TimelineItem key={index} className="timeline-item">
+                                        <TimelineSeparator>
+                                            <TimelineDot
+                                                variant="filled"
+                                                className={`timeline-dot ${screenWidth < 900 ? 'hidden-dot' : ''}`}
+                                                sx={{
+                                                    backgroundColor: theme.palette.primary.accent,
+                                                    borderColor: theme.palette.primary.accent,
+                                                    borderWidth: 1,
+                                                    borderStyle: "solid",
+                                                }}
+                                            />
+                                            {index !== steps.length - 1 && (
+                                                <TimelineConnector sx={{ bgcolor: theme.palette.primary.main }} />
+                                            )}
+                                        </TimelineSeparator>
+                                        <TimelineContent>
+                                            <div className="timeline-content">
+                                                <h2>{step.title}</h2>
+                                                <p>{step.desc}</p>
+                                            </div>
+                                        </TimelineContent>
+                                    </TimelineItem>
+                                ))}
+                            </Timeline>
+                        )}
                     </div>
                 </div>
             </div>
         </ThemeProvider>
-    )
-};
-
-
-
-// export default function HowItWorks() {
-//     const steps = [
-//         { title: "Upload your model", desc: "as a STEP from your preferred CAD software" },
-//         { title: "Choose your stock", desc: "from CloudFabs available materials" },
-//         { title: "Fixture your material", desc: "using CloudFabs proprietary software" },
-//         { title: "Program your tool paths", desc: "using industry-standard pathing" },
-//         { title: "Upload your model", desc: "as a STEP from your preferred CAD software" },
-//     ];
-
-//     return (
-//         <ThemeProvider theme={theme}>
-//             <div className="BackgroundContainer">
-//                 <div className="UIContainer">
-//                     <div className="LeftSide">
-//                         <h1>How it works</h1>
-//                     </div>
-//                     <div className="RightSide">
-//                         <Timeline position="alternate-reverse" sx={{ minHeight: '500px' }}>
-//                             {steps.map((step, index) => (
-//                                 <TimelineItem key={index} sx={{ display: 'flex', alignItems: 'center' }}>
-//                                     <TimelineSeparator>
-//                                         <TimelineDot
-//                                             variant="filled"
-//                                             sx={{
-//                                                 backgroundColor: theme.palette.primary.main,
-//                                                 borderColor: "white",
-//                                                 borderWidth: 2,
-//                                                 borderStyle: "solid",
-//                                             }}
-//                                         />
-//                                         {index !== steps.length - 1 && (
-//                                             <TimelineConnector
-//                                                 sx={{
-//                                                     bgcolor: theme.palette.primary.main,
-//                                                     flexGrow: 1,
-//                                                     width: '2px',
-//                                                     marginTop: '10px',
-//                                                 }}
-//                                             />
-//                                         )}
-//                                     </TimelineSeparator>
-//                                     <TimelineContent>
-//                                         <div className="timeline-content">
-//                                             <h2>{step.title}</h2>
-//                                             <p>{step.desc}</p>
-//                                         </div>
-//                                     </TimelineContent>
-//                                 </TimelineItem>
-//                             ))}
-//                         </Timeline>
-//                     </div>
-//                 </div>
-//             </div>
-//         </ThemeProvider>
-//     );
-// }
+    );
+}
