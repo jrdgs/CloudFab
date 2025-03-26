e4b22bd0e15ed6307fb8b79b79d4cae644c5d25b
djc2105 <DCarlson19@shschools.org>
2025-03-25 20:03:21 -0400

    Add Legal

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,9 +1,12 @@
 // App.js
 import React from 'react';
 import './styling/App.css';
-import { BrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import PrivacyPolicy from './pages/PrivacyPolicy/PrivacyPolicy';
 import Clarity from '@microsoft/clarity';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CookiePolicy from './pages/CookiePolicy/CookiePolicy';
+import Terms from './pages/Terms/Terms';
 
 const projectId = "qri40t3jag";
 Clarity.init(projectId); // Initialize Clarity only if consent is given
@@ -11,7 +14,12 @@ Clarity.init(projectId); // Initialize Clarity only if consent is given
 function App() {
   return (
     <BrowserRouter>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="cookie-policy" element={<CookiePolicy />} />
+        <Route path="terms-of-service" element={<Terms />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- landing-page/src/components/Footer/Footer.js
+++ landing-page/src/components/Footer/Footer.js
@@ -5,11 +5,11 @@ import './Footer.css';
 export default function Footer () {
     return ( 
         <div className="FooterBackground">
-            <p>Privacy Policy</p>
-            <p>Terms of service</p>
-            <p>Cookie Policy</p>
+            <a href="/privacy-policy">Privacy Policy</a>
+            <a href="/terms-of-service">Terms of service</a>
+            <a href="/cookie-policy">Cookie Policy</a>
 
-            <p>© 2025 CloudFab Inc. All rights reserved.</p>
+            <a href="/">© 2025 CloudFab Inc. All rights reserved.</a>
         </div>
     )
 }
